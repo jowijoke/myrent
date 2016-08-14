e084e775036bd5243157b4f8f758625c7c841534
John Kennedy <johnwjken@gmail.com>
2016-08-14 15:00:48 +0100

    Created ResidenceRentComparator

---
+++ app/utils/ResidenceRentComparator.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.util.Comparator;
+
+import models.Message;
+import models.Residence;
+
+public class ResidenceRentComparator implements Comparator<Residence> {
+	/**
+	 * compare the rent attributes of each Residence use the Residence compare method
+	 */
+
+	@Override
+	public int compare(Residence a, Residence b) {
+		return Integer.compare(a.rent, b.rent);
+
+	}
+}
